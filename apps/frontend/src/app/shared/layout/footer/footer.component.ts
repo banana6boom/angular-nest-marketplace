16f0ca95c879f2da7df63bbfeba02f4fb47ee968
Maksym Pastukh <saponexamq@gmail.com>
2025-02-04 21:08:46 +0100

    Настроил первоначальную архитиктуру

---
+++ apps/frontend/src/app/shared/layout/footer/footer.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-footer',
+  imports: [CommonModule],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FooterComponent {}
---
+++ apps/frontend/src/app/shared/layout/layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-layout',
+  imports: [CommonModule, HeaderComponent, FooterComponent, RouterOutlet],
+  templateUrl: './layout.component.html',
+})
+export class LayoutComponent {}
