c3c6863a7d51848fcfae19fe77898de8c998018c
Maksym Pastukh <saponexamq@gmail.com>
2025-02-05 12:46:47 +0100

    Добавил библиотеку PrimeNG

--- apps/frontend/src/app/views/main/main.component.ts
+++ apps/frontend/src/app/views/main/main.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Card } from 'primeng/card';
 
 @Component({
   selector: 'app-main',
-  imports: [CommonModule],
+  imports: [CommonModule, Card],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
