4ef96f9942b60088456b6fc91377035b99f7b1ae
Maksym Pastukh <saponexamq@gmail.com>
2025-02-10 22:28:34 +0100

    Сверстал header

--- apps/frontend/src/app/shared/layout/header/header.component.ts
+++ apps/frontend/src/app/shared/layout/header/header.component.ts
@@ -1,11 +1,38 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { IconField } from 'primeng/iconfield';
+import { InputIcon } from 'primeng/inputicon';
+import { FloatLabel } from 'primeng/floatlabel';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
+import { Button } from 'primeng/button';
+import { InputText } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterLink,
+    IconField,
+    InputIcon,
+    FloatLabel,
+    DropdownModule,
+    FormsModule,
+    InputText,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isLogged: boolean = false;
+
+  languages = [
+    { label: 'English (united States)', value: 'en-US' },
+    { label: 'Русский (Россия)', value: 'ru-RU' },
+    { label: 'Français (France)', value: 'fr-FR' },
+  ];
+
+  selectedLanguage = 'en-US';
+}
