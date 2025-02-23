d70f39548de5b2cef1298a0724b9da1783bdf549
Oleg Shchegolev <themadhouse@mail.ru>
2025-02-23 20:29:14 +0000

    feat: добавил авторизацию пользователя

---
+++ apps/backend/src/app/auth/auth.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Post, Body, Request, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UsersService } from '../users/users.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { CreateUserDto } from '../users/dto/create-user.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(
+    private authService: AuthService,
+    private usersService: UsersService,
+  ) {}
+
+  @Post('register')
+  async register(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+}
