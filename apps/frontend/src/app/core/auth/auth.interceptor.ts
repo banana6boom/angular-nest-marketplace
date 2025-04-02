398e89f45efe40b4f0a228cddd786cbb30c4d760
Maksym Pastukh <saponexamq@gmail.com>
2025-04-02 22:38:10 +0200

    Добавил HTTP-интерсептор

---
+++ apps/frontend/src/app/core/auth/auth.interceptor.ts
@@ -0,0 +1,21 @@
+import { HttpEventType, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthService } from './auth.service';
+import { tap } from 'rxjs';
+
+export const AuthInterceptor: HttpInterceptorFn = (req, next) => {
+  const token = inject(AuthService).getToken(AuthService.accessTokenKey);
+  const modifiedReq = req.clone({
+    setHeaders: {
+      Authorization: token ? `Token ${token}` : '',
+    },
+  });
+
+  return next(modifiedReq).pipe(
+    tap((event) => {
+      if (event.type === HttpEventType.Response) {
+        console.log(req.url, 'returned a response with status', event.status);
+      }
+    }),
+  );
+};
