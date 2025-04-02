35dae593e9b51822ef74f7fbee7d6531e136748c
Maksym Pastukh <saponexamq@gmail.com>
2025-04-02 20:01:40 +0200

    Небольшие правки логики и верстки

--- apps/frontend/src/app/core/auth/components/login/login.component.ts
+++ apps/frontend/src/app/core/auth/components/login/login.component.ts
@@ -1,18 +1,24 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { Toast } from 'primeng/toast';
+import { FormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule, NgOptimizedImage, RouterLink],
+  imports: [CommonModule, NgOptimizedImage, RouterLink, Toast],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginComponent {
   isPasswordVisible: boolean = false;
+  formLogin!: FormGroup;
+  private subscription: Subscription | null = null;
 
   togglePasswordVisibility() {
     this.isPasswordVisible = !this.isPasswordVisible;
   }
+
 }
