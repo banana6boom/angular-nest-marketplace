0e4a4e51e3077fbc0c32d8972ae97ccc3bd1ff9d
Maksym Pastukh <saponexamq@gmail.com>
2025-04-05 17:04:54 +0200

    Добавил слайдер для главной страницы

--- apps/frontend/src/app/views/main/main.component.ts
+++ apps/frontend/src/app/views/main/main.component.ts
@@ -1,11 +1,38 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Carousel } from 'primeng/carousel';
+import { GalleriaModule } from 'primeng/galleria';
+import { Button } from 'primeng/button';
 
 @Component({
   selector: 'app-main',
-  imports: [CommonModule],
+  imports: [CommonModule, Carousel, GalleriaModule, Button],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MainComponent {}
+export class MainComponent implements OnInit {
+  // В компоненте
+  slides = [
+    {
+      image: 'assets/images/shop-slide-1.png',
+      category: 'T-Shirt / Tops',
+      title: 'Summer Value Pack',
+      description: 'cool / colorful / comfy',
+    },
+    {
+      image: 'assets/images/shop-slide-2.webp',
+      category: 'Jackets / Coats',
+      title: 'Winter Collection',
+      description: 'warm / stylish / trendy',
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit() {}
+}
