1404f34916615c32f5ff84bcb4a63711e39205f5
Maksym Pastukh <saponexamq@gmail.com>
2025-05-13 21:42:58 +0200

    Реализация стилей для фильтрации

--- apps/frontend/src/app/shared/components/product-filter/product-filter.component.ts
+++ apps/frontend/src/app/shared/components/product-filter/product-filter.component.ts
@@ -1,6 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Accordion, AccordionContent, AccordionHeader, AccordionPanel } from 'primeng/accordion';
+import { OverlayPanel, OverlayPanelModule } from 'primeng/overlaypanel';
+import { TieredMenu } from 'primeng/tieredmenu';
 
 @Component({
   selector: 'app-product-filter',
@@ -8,11 +10,44 @@ import { Accordion, AccordionContent, AccordionHeader, AccordionPanel } from 'pr
     CommonModule,
     Accordion,
     AccordionPanel,
-    AccordionHeader,
     AccordionContent,
+    AccordionHeader,
+    OverlayPanelModule,
+    TieredMenu,
   ],
   templateUrl: './product-filter.component.html',
   styleUrl: './product-filter.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductFilterComponent {}
+export class ProductFilterComponent {
+  @ViewChild('op') overlay!: OverlayPanel;
+
+  // В вашем компоненте
+  items = [
+    {
+      label: 'Tops',
+      items: [
+        {label: 'Basic Tops'},
+        {label: 'Designer Tops'},
+        {
+          label: 'Sports',
+          items: [
+            {label: 'Tank Tops'},
+            {label: 'Training Tops'}
+          ]
+        }
+      ]
+    }
+  ];
+
+  timeout: any;
+
+  showOverlay(event: MouseEvent) {
+    clearTimeout(this.timeout);
+    this.overlay.show(event);
+  }
+
+  hideOverlay() {
+    this.timeout = setTimeout(() => this.overlay.hide(), 11200);
+  }
+}
