8c5a1300df072095102084809412a52d49663171
Maksym Pastukh <saponexamq@gmail.com>
2025-05-18 14:55:14 +0200

    Доработал стили компонента фильтрации

--- apps/frontend/src/app/shared/components/product-filter/product-filter.component.ts
+++ apps/frontend/src/app/shared/components/product-filter/product-filter.component.ts
@@ -17,6 +17,9 @@ import { OverlayPanel, OverlayPanelModule } from 'primeng/overlaypanel';
 import { TieredMenu } from 'primeng/tieredmenu';
 import { Badge } from 'primeng/badge';
 import { MenuItem } from 'primeng/api';
+import { min } from 'rxjs';
+import { Slider } from 'primeng/slider';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-filter',
@@ -28,12 +31,38 @@ import { MenuItem } from 'primeng/api';
     AccordionHeader,
     OverlayPanelModule,
     TieredMenu,
+    Slider,
+    FormsModule,
   ],
   templateUrl: './product-filter.component.html',
   styleUrl: './product-filter.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductFilterComponent {
+  priceRange: number[] = [70, 270];
+  min = 0;
+  max = 400;
+
+  sizes: string[] = ['XXS', 'XS', 'S', 'M', 'L', 'XL', 'XXL', '3XL', '4XL'];
+  selectedSizes: string[] = [];
+
+  selectorColor: string | null = null;
+
+  colors = [
+    { name: 'Purple', value: '#8e44ad' },
+    { name: 'Black', value: '#000000' },
+    { name: 'Red', value: '#e74c3c' },
+    { name: 'Orange', value: '#e67e22' },
+    { name: 'Navy', value: '#2980b9' },
+    { name: 'White', value: '#ffffff' },
+    { name: 'Broom', value: '#d35400' },
+    { name: 'Green', value: '#2ecc71' },
+    { name: 'Yellow', value: '#f1c40f' },
+    { name: 'Grey', value: '#bdc3c7' },
+    { name: 'Pink', value: '#e91e63' },
+    { name: 'Blue', value: '#00bcd4' },
+  ];
+
   items: MenuItem[] = [
     {
       label: 'Tops',
@@ -73,14 +102,54 @@ export class ProductFilterComponent {
     },
   ];
 
-  openedPanels: string[] = ['0'];
+  itemsStyle: MenuItem[] = [
+    {
+      label: 'Classic',
+      items: [{ label: 'Brand A' }, { label: 'Brand B' }, { label: 'Brand C' }],
+    },
+    {
+      label: 'Casual',
+      items: [{ label: 'Brand A' }, { label: 'Brand B' }, { label: 'Brand C' }],
+    },
+    {
+      label: 'Business',
+      items: [{ label: 'Brand A' }, { label: 'Brand B' }, { label: 'Brand C' }],
+    },
+    {
+      label: 'Sport',
+      items: [{ label: 'Brand A' }, { label: 'Brand B' }, { label: 'Brand C' }],
+    },
+    {
+      label: 'Elegant',
+      items: [{ label: 'Brand A' }, { label: 'Brand B' }, { label: 'Brand C' }],
+    },
+    {
+      label: 'Formal (evening)',
+      items: [{ label: 'Brand A' }, { label: 'Brand B' }, { label: 'Brand C' }],
+    },
+  ];
 
-  onAccordionChange(nextValues: any) {
+  openedPanels: string[] = ['0', '1', '2', '3', '4'];
+
+  onAccordionChange(nextValues: any): void {
     const values = Array.isArray(nextValues) ? nextValues : [nextValues];
-    if (!values.includes('0')) {
-      this.openedPanels = ['0', ...values];
+    if (!values.includes('0') || !values.includes('4')) {
+      this.openedPanels = ['0', '4', ...values];
     } else {
       this.openedPanels = values;
+    }
+  }
+
+  selectColor(color: string): void {
+    this.selectorColor = this.selectorColor === color ? null : color;
+  }
+
+  toggleSize(size: string): void {
+    const index: number = this.selectedSizes.indexOf(size);
+    if (index > -1) {
+      this.selectedSizes.splice(index, 1);
+    } else {
+      this.selectedSizes.push(size);
     }
   }
 }
